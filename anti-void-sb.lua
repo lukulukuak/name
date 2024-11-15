6b2e744ec5ac7a00a0b5669e795d0a91937135d4
lukulukuak <lakurtlutepe@gmail.com>
2024-11-15 18:38:23 +0100

    Update anti-void-sb.lua

--- anti-void-sb.lua
+++ anti-void-sb.lua
@@ -1,24 +1,7 @@
 local plr = game.Players.LocalPlayer
-local ui = Instance.new("ScreenGui", plr.PlayerGui)
-local btn = Instance.new("TextButton", ui)
-local info = Instance.new("TextLabel", btn)
 local chr = plr.Character
 local hrp = chr:WaitForChild("HumanoidRootPart")
 local on = false
-btn.Size = UDim2.new(0.1,0, 0.1, 0)
-btn.TextScaled = true
-btn.BackgroundColor3 = Color3.fromRGB(68, 34, 34)
-btn.TextColor3 = Color3.fromRGB(255, 215, 215)
-btn.Text = "Float"
-btn.Position = UDim2.new(0.45, 0, 0, 0)
-info.Text = "B"
-info.TextScaled = true
-info.BackgroundColor3 = btn.BackgroundColor3
-info.TextColor3 = btn.TextColor3
-info.Size = UDim2.new(1,0,1,0)
-info.Position = UDim2.new(0, 0, 1, 0)
-ui.ResetOnSpawn = false
-ui.IgnoreGuiInset = false
 
 function create()
 	local part = Instance.new("Part", workspace)
@@ -50,18 +33,10 @@ end
 function click()
 	if on == false then
 		on = true
-		btn.BackgroundColor3 = Color3.fromRGB(34, 47, 34)
-		btn.TextColor3 = Color3.fromRGB(215, 255, 215)
-		info.BackgroundColor3 = btn.BackgroundColor3
-		info.TextColor3 = btn.TextColor3
 		create()
 	end
 	if on == true then
 		on = false
-		btn.BackgroundColor3 = Color3.fromRGB(47, 34, 34)
-		btn.TextColor3 = Color3.fromRGB(255, 215, 215)
-		info.BackgroundColor3 = btn.BackgroundColor3
-		info.TextColor3 = btn.TextColor3
 		remove()
 	end
 end
