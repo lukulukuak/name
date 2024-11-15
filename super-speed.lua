5a3b3ee19b1d5df41c4c2e082695660dc849c7df
lukulukuak <lakurtlutepe@gmail.com>
2024-11-15 18:42:36 +0100

    Update super-speed.lua

--- super-speed.lua
+++ super-speed.lua
@@ -1,8 +1,15 @@
 local plr = game.Players.LocalPlayer
 local chr = plr.Character or plr.CharacterAdded:Wait()
+local hmd = chr:WaitForChild("Humanoid")
 
-
+game:GetService("UserInputService").InputBegan(Connect(function(i)
+	if i.KeyCode == Enum.KeyCode.V then
+		hmd.WalkSpeed = hmd.WalkSpeed == 64 and 16 or 64
+	end
+end)
 
 plr.CharacterAdded:Connect(function(ch)
 	chr = ch
+	hmd = ch:WaitForChild("Humanoid")
 end)
+
