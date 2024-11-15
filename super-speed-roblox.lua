578aa5db46fdf4b36372e8fdbeb22777e21a8d68
lukulukuak <lakurtlutepe@gmail.com>
2024-11-15 18:53:01 +0100

    Update super-speed-roblox.lua

--- super-speed-roblox.lua
+++ super-speed-roblox.lua
@@ -2,7 +2,7 @@ local plr = game.Players.LocalPlayer
 local chr = plr.Character or plr.CharacterAdded:Wait()
 local hmd = chr:WaitForChild("Humanoid")
 
-game:GetService("UserInputService").InputBegan(Connect(function(i)
+game:GetService("UserInputService").InputBegan:Connect(function(i)
 	if i.KeyCode == Enum.KeyCode.V then
 		hmd.WalkSpeed = hmd.WalkSpeed == 64 and 16 or 64
 	end
