4e1c5169b2a4a562602c1a225ba8f2001e9a36dc
lukulukuak <lakurtlutepe@gmail.com>
2024-11-15 19:44:51 +0100

    Update esp-roblox.lua

--- esp-roblox.lua
+++ esp-roblox.lua
@@ -7,66 +7,100 @@ plr.CharacterAdded:Connect(function(ch)
 end)
 
 for _, plyr in ipairs(players) do
-    if not plyr.Character then plyr.CharacterAdded:Wait() end
-    local gui = Instance.new("BillboardGui", plyr.Character)
-    local name = Instance.new("TextLabel", gui)
-    local dist = Instance.new("TextLabel", gui)
-    local dista = (plyr.Character.HumanoidRootPart.Position - chr.HumanoidRootPart.Position).Magnitude
-    local str1, str2 = Instance.new("UIStroke", dist), Instance.new("UIStroke", name)
-    str1.Thickness = 2
-    str2.Thickness = 2
-    name.Size = UDim2.new(1, 0, 0.5, 0)
-    name.Text = plyr.Name
-    name.BackgroundTransparency = 1
-    name.TextColor3 = Color3.new(1,1,1)
-    name.TextScaled = true
-    dist.Size = UDim2.new(1, 0, 0.5, 0)
-    dist.Position = UDim2.new(0, 0, 0.5, 0)
-    dist.Text = "Distance:" .. dista
-    dist.BackgroundTransparency = 1
-    dist.TextColor3 = Color3.new(1,1,1)
-    dist.TextScaled = true
-    gui.ResetOnSpawn = false
-    gui.AlwaysOnTop = true
-    gui.LightInfluence = 0
-    gui.Size = UDim2.new(5, 0, 2, 0)
-    gui.Enabled = false
-    gui.Name = "esp"
+    if plyr ~= plr then
+        if not plyr.Character then plyr.CharacterAdded:Wait() end
+        local gui = Instance.new("BillboardGui", plyr.Character)
+        local name = Instance.new("TextLabel", gui)
+        local dist = Instance.new("TextLabel", gui)
+        local dista = (plyr.Character.HumanoidRootPart.Position - chr.HumanoidRootPart.Position).Magnitude
+        local str1, str2 = Instance.new("UIStroke", dist), Instance.new("UIStroke", name)
+        str1.Thickness = 2
+        str2.Thickness = 2
+        name.Size = UDim2.new(1, 0, 0.5, 0)
+        name.Text = plyr.Name
+        name.BackgroundTransparency = 1
+        name.TextColor3 = Color3.new(1,1,1)
+        name.TextScaled = true
+        dist.Size = UDim2.new(1, 0, 0.5, 0)
+        dist.Position = UDim2.new(0, 0, 0.5, 0)
+        dist.Text = "Distance:" .. dista
+        dist.BackgroundTransparency = 1
+        dist.TextColor3 = Color3.new(1,1,1)
+        dist.TextScaled = true
+        gui.ResetOnSpawn = false
+        gui.AlwaysOnTop = true
+        gui.LightInfluence = 0
+        gui.Size = UDim2.new(5, 0, 2, 0)
+        gui.Enabled = false
+        gui.Name = "esp"
+    end
 end
 
 game.Players.PlayerAdded:Connect(function(plyr)
-    if not plyr.Character then plyr.CharacterAdded:Wait() end
-    local gui = Instance.new("BillboardGui", plyr.Character)
-    local name = Instance.new("TextLabel", gui)
-    local dist = Instance.new("TextLabel", gui)
-    local dista = (plyr.Character.HumanoidRootPart.Position - chr.HumanoidRootPart.Position).Magnitude
-    local str1, str2 = Instance.new("UIStroke", dist), Instance.new("UIStroke", name)
-    str1.Thickness = 2
-    str2.Thickness = 2
-    name.Size = UDim2.new(1, 0, 0.5, 0)
-    name.Text = plyr.Name
-    name.BackgroundTransparency = 1
-    name.TextColor3 = Color3.new(1,1,1)
-    name.TextScaled = true
-    dist.Size = UDim2.new(1, 0, 0.5, 0)
-    dist.Position = UDim2.new(0, 0, 0.5, 0)
-    dist.Text = "Distance:" .. dista
-    dist.BackgroundTransparency = 1
-    dist.TextColor3 = Color3.new(1,1,1)
-    dist.TextScaled = true
-    gui.ResetOnSpawn = false
-    gui.AlwaysOnTop = true
-    gui.LightInfluence = 0
-    gui.Size = UDim2.new(5, 0, 2, 0)
-    gui.Enabled = false
-    gui.Name = "esp"
-    table.insert(players, plyr)
+    if plyr ~= plr then
+        if not plyr.Character then plyr.CharacterAdded:Wait() end
+        local gui = Instance.new("BillboardGui", plyr.Character)
+        local name = Instance.new("TextLabel", gui)
+        local dist = Instance.new("TextLabel", gui)
+        local dista = (plyr.Character.HumanoidRootPart.Position - chr.HumanoidRootPart.Position).Magnitude
+        local str1, str2 = Instance.new("UIStroke", dist), Instance.new("UIStroke", name)
+        str1.Thickness = 2
+        str2.Thickness = 2
+        name.Size = UDim2.new(1, 0, 0.5, 0)
+        name.Text = plyr.Name
+        name.BackgroundTransparency = 1
+        name.TextColor3 = Color3.new(1,1,1)
+        name.TextScaled = true
+        dist.Size = UDim2.new(1, 0, 0.5, 0)
+        dist.Position = UDim2.new(0, 0, 0.5, 0)
+        dist.Text = "Distance:" .. dista
+        dist.BackgroundTransparency = 1
+        dist.TextColor3 = Color3.new(1,1,1)
+        dist.TextScaled = true
+        gui.ResetOnSpawn = false
+        gui.AlwaysOnTop = true
+        gui.LightInfluence = 0
+        gui.Size = UDim2.new(5, 0, 2, 0)
+        gui.Enabled = false
+        gui.Name = "esp"
+        table.insert(players, plyr)
+    end
 end)
 
 game:GetService("UserInputService").InputBegan:Connect(function(i)
     if i.KeyCode == Enum.KeyCode.N then
         for _, plyr in ipairs(players) do
-            plyr.esp.Enabled = true
+            if plyr:FindFirstChild("esp") and plyr:FindFirstChild("esp"):IsA("BillboardGui") then
+                plyr.esp.Enabled = not plyr.esp.Enabled
+            elseif not plyr:FindFirstChild("esp") then
+                if plyr ~= plr then
+                    if not plyr.Character then plyr.CharacterAdded:Wait() end
+                    local gui = Instance.new("BillboardGui", plyr.Character)
+                    local name = Instance.new("TextLabel", gui)
+                    local dist = Instance.new("TextLabel", gui)
+                    local dista = (plyr.Character.HumanoidRootPart.Position - chr.HumanoidRootPart.Position).Magnitude
+                    local str1, str2 = Instance.new("UIStroke", dist), Instance.new("UIStroke", name)
+                    str1.Thickness = 2
+                    str2.Thickness = 2
+                    name.Size = UDim2.new(1, 0, 0.5, 0)
+                    name.Text = plyr.Name
+                    name.BackgroundTransparency = 1
+                    name.TextColor3 = Color3.new(1,1,1)
+                    name.TextScaled = true
+                    dist.Size = UDim2.new(1, 0, 0.5, 0)
+                    dist.Position = UDim2.new(0, 0, 0.5, 0)
+                    dist.Text = "Distance:" .. dista
+                    dist.BackgroundTransparency = 1
+                    dist.TextColor3 = Color3.new(1,1,1)
+                    dist.TextScaled = true
+                    gui.ResetOnSpawn = false
+                    gui.AlwaysOnTop = true
+                    gui.LightInfluence = 0
+                    gui.Size = UDim2.new(5, 0, 2, 0)
+                    gui.Enabled = false
+                    gui.Name = "esp"
+                end     
+            end
         end
     end
 end)
